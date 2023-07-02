bff95decea464a08cf3c721a96d997e2f9a57ae0
Jesús Muñoz <69815798+JeMuGa@users.noreply.github.com>
2023-07-02 19:55:34 +0200

    problem 12

--- README.md
+++ README.md
@@ -173,3 +173,16 @@ Implement a job scheduler which takes in a function f and an integer n, and call
 
 
 ---
+
+## Problem 12
+
+Implement an autocomplete system. That is, given a query string s and a set of all possible query strings,
+return all strings in the set that have s as a prefix.
+
+For example, given the query string de and the set of strings [dog, deer, deal], return [deer, deal].
+
+Hint: Try preprocessing the dictionary into a more efficient data structure to speed up queries.
+
+[Solution](https://github.com/JeMuGa/coding_problems/blob/main/problems/11-20/problem_12.js)
+
+---
--- tests/11-20.spec.js
+++ tests/11-20.spec.js
@@ -1,4 +1,7 @@
+import isPermutation from "../problems/helpers/isPermutation";
+
 import schedule from "../problems/11-20/problem_11";
+import getWordsWithPrefix from "../problems/11-20/problem_12";
 
 describe('Problems 1 to 10', () => {
   test('11: Job Scheduling', () => {
@@ -17,6 +20,33 @@ describe('Problems 1 to 10', () => {
 
     expect(setTimeout).toHaveBeenCalledTimes(1);
     expect(setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 1000);
+  });
+
+  test('12: Get All Words With Prefix', () => {
+    expect(
+      isPermutation(getWordsWithPrefix('de', ['dog', 'deer', 'deal']), [
+        'deer',
+        'deal'
+      ])
+    ).toBe(true);
+
+    expect(
+      isPermutation(getWordsWithPrefix('d', ['dog', 'deer', 'deal', 'cat']), [
+        'dog',
+        'deer',
+        'deal'
+      ])
+    ).toBe(true);
+
+    expect(getWordsWithPrefix('cat', ['dog', 'deer', 'deal', 'cat'])).toEqual([
+      'cat'
+    ]);
+
+    expect(getWordsWithPrefix('f', ['dog', 'deer', 'deal', 'cat'])).toEqual([]);
+
+    expect(getWordsWithPrefix('fr', ['dog', 'deer', 'deal', 'cat'])).toEqual(
+      []
+    );
   });
 });
 
