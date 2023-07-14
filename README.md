4f301eb032ddd1c65fb3d4ff5e74ecc80856f841
Jesús Muñoz <69815798+JeMuGa@users.noreply.github.com>
2023-07-14 17:55:57 +0200

    problem 23

--- README.md
+++ README.md
@@ -360,3 +360,19 @@ For example, given [(30, 75), (0, 50), (60, 150)], you should return 2.
 
 ---
 
+## Problem 23
+
+Given a dictionary of words and a string made up of those words (no spaces), return the original sentence in a list.
+If there is more than one possible reconstruction, return any of them.
+If there is no possible reconstruction, then return null.
+
+For example, given the set of words 'quick', 'brown', 'the', 'fox', and the string "thequickbrownfox",
+you should return ['the', 'quick', 'brown', 'fox'].
+
+Given the set of words 'bed', 'bath', 'bedbath', 'and', 'beyond', and the string "bedbathandbeyond",
+return either ['bed', 'bath', 'and', 'beyond] or ['bedbath', 'and', 'beyond'].
+
+[Solution](https://github.com/JeMuGa/coding_problems/blob/main/problems/21-30/problem_23.js)
+
+---
+
--- tests/21-30.spec.js
+++ tests/21-30.spec.js
@@ -1,7 +1,9 @@
+import isEqual from "lodash.isequal";
 import LinkedListNode from "../problems/data-structures/LinkedListNode";
 
 import findIntersection from "../problems/21-30/problem_21";
 import minRooms from "../problems/21-30/problem_22";
+import wordBreak from "../problems/21-30/problem_23";
 
 describe('Problems 21 to 30', () => {
   test('21: Intersection of Two Linked Lists', () => {
@@ -70,5 +72,38 @@ describe('Problems 21 to 30', () => {
     expect(minRooms([[0, 50], [0, 10], [0, 20]])).toBe(3);
     expect(minRooms([[0, 30], [30, 40], [40, 50]])).toBe(1);
     expect(minRooms([])).toBe(0);
+  });
+
+  test('23: Build String', () => {
+    expect(
+      wordBreak('thequickbrownfox', ['the', 'quick', 'brown', 'fox'])
+    ).toEqual(['the', 'quick', 'brown', 'fox']);
+
+    expect(
+      wordBreak('thequickbrownfoxx', ['the', 'quick', 'brown', 'fox'])
+    ).toBeNull();
+
+    expect(
+      isEqual(
+        wordBreak('bedbathandbeyond', [
+          'bed',
+          'bath',
+          'bedbath',
+          'and',
+          'beyond'
+        ]),
+        ['bed', 'bath', 'and', 'beyond']
+      ) ||
+        isEqual(
+          wordBreak('bedbathandbeyond', [
+            'bed',
+            'bath',
+            'bedbath',
+            'and',
+            'beyond'
+          ]),
+          ['bedbath', 'and', 'beyond']
+        )
+    ).toBe(true);
   });
 });
