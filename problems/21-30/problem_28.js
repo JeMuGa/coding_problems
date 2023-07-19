a9780406b11fd40d39464eee9ccdeac65f673639
Jesús Muñoz <69815798+JeMuGa@users.noreply.github.com>
2023-07-19 17:40:11 +0200

    problem 28

---
+++ problems/21-30/problem_28.js
@@ -0,0 +1,39 @@
+// Problem 28
+//
+// Given a string of round, curly, and square open and closing brackets, return whether the brackets are balanced (well-formed).
+//
+// For example, given the string "([])[]({})", you should return true.
+//
+// Given the string "([)]" or "((()", you should return false.
+//
+// O(N) Time and Space Complexity
+// N is the length of the string
+
+function validBrackets(string) {
+  const stack = [];
+  for (let i = 0; i < string.length; i++) {
+    const bracket = string.charAt(i);
+
+    // push opening brackets onto stack
+    if (bracket === '(' || bracket === '[' || bracket === '{')
+      stack.push(bracket);
+    else {
+      // closing brackets
+      const openingBracket = stack.pop();
+      if (openingBracket === undefined) return false;
+      if (!isMatch(openingBracket, bracket)) return false;
+    }
+  }
+
+  return stack.length === 0;
+}
+
+function isMatch(opening, closing) {
+  return (
+    (opening === '(' && closing === ')') ||
+    (opening === '[' && closing === ']') ||
+    (opening === '{' && closing === '}')
+  );
+}
+
+export default validBrackets;
