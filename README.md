a29ab4203e2661d7585494d17e63c4e6d2e048e8
Jesús Muñoz <69815798+JeMuGa@users.noreply.github.com>
2023-07-23 21:09:34 +0200

    problem 32

--- README.md
+++ README.md
@@ -516,3 +516,14 @@ Given the input [3, 0, 1, 3, 0, 5], we can hold 3 units in the first index, 2 in
 
 ---
 
+## Problem 32
+
+The edit distance between two strings refers to the minimum number of character insertions, deletions, and substitutions required to change one string to the other.
+
+For example, the edit distance between “kitten” and “sitting” is three: substitute the “k” for “s”, substitute the “e” for “i”, and append a “g”.
+
+Given two strings, compute the edit distance between them.
+
+[Solution](https://github.com/JeMuGa/coding_problems/blob/main/problems/31-40/problem_32.js)
+
+---
--- tests/31-40.spec.js
+++ tests/31-40.spec.js
@@ -1,9 +1,19 @@
 import trappingRainWater from "../problems/31-40/problem_31";
+import editDistance from "../problems/31-40/problem_32";
 
 describe('Problems 31 to 40', () => {
   test('31: Trapping Rain Water', () => {
     expect(trappingRainWater([2, 1, 2])).toBe(1);
     expect(trappingRainWater([3, 0, 1, 3, 0, 5])).toBe(8);
     expect(trappingRainWater([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1])).toBe(6);
+  });
+
+  test('32: Edit Distance', () => {
+    expect(editDistance('kitten', 'sitting')).toBe(3);
+    expect(editDistance('', '')).toBe(0);
+    expect(editDistance('', 'abcd')).toBe(4);
+    expect(editDistance('abcd', 'abcd')).toBe(0);
+    expect(editDistance('horse', 'ros')).toBe(3);
+    expect(editDistance('intention', 'execution')).toBe(5);
   });
 });
