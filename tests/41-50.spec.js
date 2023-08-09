63a23e9e66599d17801c810c547dd623b7fbb015
JeMuGa <jesus.munoz.garrido@gmail.com>
2023-08-09 02:08:32 +0200

    problem 42

--- tests/41-50.spec.js
+++ tests/41-50.spec.js
@@ -1,8 +1,46 @@
 import singleNumber from "../problems/41-50/problem_41";
+import constructItinerary from "../problems/41-50/problem_42";
 
 describe('Problems 41 to 50', () => {
   test('41: Single Number', () => {
     expect(singleNumber([6, 1, 3, 3, 3, 6, 6])).toBe(1);
     expect(singleNumber([13, 19, 13, 13])).toBe(19);
+  });
+
+  test('42: Reconstruct Itinerary', () => {
+    expect(
+      constructItinerary(
+        [['SFO', 'HKO'], ['YYZ', 'SFO'], ['YUL', 'YYZ'], ['HKO', 'ORD']],
+        'YUL'
+      )
+    ).toEqual(['YUL', 'YYZ', 'SFO', 'HKO', 'ORD']);
+
+    expect(
+      constructItinerary([['SFO', 'COM'], ['COM', 'YYZ']], 'COM')
+    ).toBeNull();
+
+    expect(
+      constructItinerary([['A', 'B'], ['A', 'C'], ['B', 'C'], ['C', 'A']], 'A')
+    ).toEqual(['A', 'B', 'C', 'A', 'C']);
+
+    expect(
+      constructItinerary(
+        [['MUC', 'LHR'], ['JFK', 'MUC'], ['SFO', 'SJC'], ['LHR', 'SFO']],
+        'JFK'
+      )
+    ).toEqual(['JFK', 'MUC', 'LHR', 'SFO', 'SJC']);
+
+    expect(
+      constructItinerary(
+        [
+          ['JFK', 'SFO'],
+          ['JFK', 'ATL'],
+          ['SFO', 'ATL'],
+          ['ATL', 'JFK'],
+          ['ATL', 'SFO']
+        ],
+        'JFK'
+      )
+    ).toEqual(['JFK', 'ATL', 'JFK', 'SFO', 'ATL', 'SFO']);
   });
 });
