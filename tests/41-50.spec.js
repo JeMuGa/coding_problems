5d3630293348214d3d61a7073e1c43a5998e2d3d
Jesús Muñoz <69815798+JeMuGa@users.noreply.github.com>
2023-08-09 02:13:53 +0200

    problem 43

--- tests/41-50.spec.js
+++ tests/41-50.spec.js
@@ -1,5 +1,6 @@
 import singleNumber from "../problems/41-50/problem_41";
 import constructItinerary from "../problems/41-50/problem_42";
+import subsetSum from "../problems/41-50/problem_43";
 
 describe('Problems 41 to 50', () => {
   test('41: Single Number', () => {
@@ -42,5 +43,30 @@ describe('Problems 41 to 50', () => {
         'JFK'
       )
     ).toEqual(['JFK', 'ATL', 'JFK', 'SFO', 'ATL', 'SFO']);
+  });
+
+  test('43: Subset Sum', () => {
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 24)).toEqual([12, 1, 9, 2]);
+
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 15)).toEqual([1, 5, 9]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 16)).toEqual([5, 9, 2]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 17)).toEqual([12, 5]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 18)).toEqual([12, 1, 5]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 19)).toEqual([12, 5, 2]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 20)).toEqual([12, 1, 5, 2]);
+
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 21)).toEqual([12, 9]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 22)).toEqual([12, 1, 9]);
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 23)).toEqual([12, 9, 2]);
+
+    expect(subsetSum([12, 1, 61, 5, 9, 2], 25)).toBeNull();
+
+    expect(subsetSum([2, 3, 7, 8, 10], 9)).toEqual([2, 7]);
+    expect(subsetSum([2, 3, 7, 8, 10], 10)).toEqual([10]);
+    expect(subsetSum([2, 3, 7, 8, 10], 11)).toEqual([3, 8]);
+
+    expect(subsetSum([2, 3, 7, 8, 10], 30)).toEqual([2, 3, 7, 8, 10]);
+
+    expect(subsetSum([2, 3, 7, 8, 10], 14)).toBeNull();
   });
 });
